4c272ac9085cbb8cf9a4a5906e4768bf78e723dd
luongcongduy97 <duylcong97@gmail.com>
2024-09-08 22:29:47 +0700

    Add real time task page

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,9 @@
 
 class Task < ApplicationRecord
   validates :name, presence: true
+
+  after_create_commit lambda {
+                        broadcast_prepend_to 'tasks', partial: 'tasks/task', locals: { quote: self },
+                                                      target: 'tasks'
+                      }
 end
