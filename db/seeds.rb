6ded4e83b4c571a81708d0126a681a42c0e6d3bf
luongcongduy97 <duylcong97@gmail.com>
2024-09-11 19:29:17 +0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Clear existing data to avoid duplication
+Task.destroy_all
+User.destroy_all
+Company.destroy_all
+
+5.times do
+  company = Company.create!(
+    name: Faker::Company.name
+  )
+
+  3.times do
+    User.create!(
+      email: Faker::Internet.email,
+      password: 'password123',
+      company:
+    )
+
+    5.times do
+      Task.create!(
+        name: Faker::Lorem.sentence(word_count: 3),
+        description: Faker::Lorem.paragraph(sentence_count: 2),
+        company:
+      )
+    end
+  end
+end
